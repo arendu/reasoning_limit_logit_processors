a0a1a8f8035d741a1977b98efe68cf73b03d6aed
root <root@pool0-01101.cm.cluster>
2026-03-09 21:42:49 -0700

    added qwen3 support
    Signed-off-by: root <root@pool0-01101.cm.cluster>

--- get_token_ids.py
+++ get_token_ids.py
@@ -24,6 +24,7 @@ def phrase_to_ids(phrase: str, model_name: str = "Qwen/Qwen3-8B"):
 if __name__ == "__main__":
     # Example phrases
     model_name = "nvidia/NVIDIA-Nemotron-Nano-12B-v2" # you can replace with other models from HF Qwen/Qwen3-8B for example...
+    model_name = "Qwen/Qwen3-8B"
     print(model_name)
     phrase_to_ids(" Reached thinking limit. </think>", model_name)
     phrase_to_ids(" </think>", model_name)
---
+++ serve_qwen3.sh
@@ -0,0 +1,7 @@
+export THINKING_BUDGET_LOGITS_PROCESSOR_ARGS='{"thinking_budget": 150, "thinking_budget_grace_period": 30, "end_token_ids": " Reached thinking limit. </think>", "end_think_ids": ["</think>"], "prompt_think_ids": "<think>", "model": "Qwen/Qwen3-8B"}'
+python3 -m vllm.entrypoints.openai.api_server \
+	--served-model-name "model"  \
+	--model Qwen/Qwen3-8B \
+	--logits-processors "custom_logit_processors.v1.qwen3_logit_processors:ThinkingBudgetLogitsProcessor" \
+	--port 8881 \
+	--trust-remote-code 
